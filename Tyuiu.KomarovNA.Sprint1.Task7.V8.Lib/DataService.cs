1e3068ff4c61245bf75ff73f34f572ac31c1c258
gravity34031 <gravity2507@gmail.com>
2024-09-21 03:25:28 +0500

    Task7 fixed

--- Tyuiu.KomarovNA.Sprint1.Task7.V8.Lib/DataService.cs
+++ Tyuiu.KomarovNA.Sprint1.Task7.V8.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.KomarovNA.Sprint1.Task7.V8.Lib
     {
         public double Calculate(double x, double y)
         {
-            return x*Math.Log(x) + y/(Math.Cos(x) - x/3);
+            return Math.Round(x*Math.Log(x) + y/(Math.Cos(x) - x/3), 3);
         }
     }
 }

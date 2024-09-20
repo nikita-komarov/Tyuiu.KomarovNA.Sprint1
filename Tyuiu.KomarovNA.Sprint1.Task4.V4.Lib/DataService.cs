a8a543e6492356607b0b647ffcb1ec2a0cc8c90b
gravity34031 <gravity2507@gmail.com>
2024-09-21 02:34:38 +0500

    Task4

---
+++ Tyuiu.KomarovNA.Sprint1.Task4.V4.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KomarovNA.Sprint1.Task4.V4.Lib
+{
+    public class DataService : ISprint1Task4V4
+    {
+        public double Calculate(double x, double y)
+        {
+            return Math.Round((double)((1 + x * y) / (Math.Abs(x + 2))), 3);
+        }
+    }
+}
---
+++ Tyuiu.KomarovNA.Sprint1.Task4.V4/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.KomarovNA.Sprint1.Task4.V4.Lib;
+
+namespace Tyuiu.KomarovNA.Sprint1.Task4.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Class Math                                                              *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Написать программу, которая запрашивает у пользователя исходные данные, в*");
+            Console.WriteLine("*вычисляет результат по формуле и печатает его на экране. Ответ округлите *");
+            Console.WriteLine("* до 3 знаков после запятой. Формула: (1+xy) / (|x+2|)                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            double x, y;
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            double result = ds.Calculate(x, y);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}

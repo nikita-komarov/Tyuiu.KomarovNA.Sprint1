b15dc95d97adeaf55c698d0f92bb690157da4a3c
gravity34031 <gravity2507@gmail.com>
2024-09-21 02:48:49 +0500

    Task5

---
+++ Tyuiu.KomarovNA.Sprint1.Task5.V3.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KomarovNA.Sprint1.Task5.V3.Lib
+{
+    public class DataService : ISprint1Task5V3
+    {
+        public int Calculate(int k)
+        {
+
+            return (int)((k % 1000) / 100);
+        }
+    }
+}
---
+++ Tyuiu.KomarovNA.Sprint1.Task5.V3/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KomarovNA.Sprint1.Task5.V3.Lib;
+
+
+namespace Tyuiu.KomarovNA.Sprint1.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Преобразование типов и класс Convert                                    *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Написать программу, которая решает следующую задачу: Присвоить целой пере*");
+            Console.WriteLine("*еменной h третью от конца цифру в записи положительного целого числа k (н*");
+            Console.WriteLine("*например, если k=130985, то h=9).                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            int k = int.Parse(Console.ReadLine());
+            int result = ds.Calculate(k);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}

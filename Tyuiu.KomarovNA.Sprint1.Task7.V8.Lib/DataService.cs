086a75866b1d76bd88a01c168c2be18aa195344c
gravity34031 <gravity2507@gmail.com>
2024-09-21 03:23:41 +0500

    Task7

---
+++ Tyuiu.KomarovNA.Sprint1.Task7.V8.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KomarovNA.Sprint1.Task7.V8.Lib
+{
+    public class DataService : ISprint1Task7V8
+    {
+        public double Calculate(double x, double y)
+        {
+            return x*Math.Log(x) + y/(Math.Cos(x) - x/3);
+        }
+    }
+}
---
+++ Tyuiu.KomarovNA.Sprint1.Task7.V8/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KomarovNA.Sprint1.Task7.V8.Lib;
+namespace Tyuiu.KomarovNA.Sprint1.Task7.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Добавление к решению итоговых проектов по спринту                       *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Написать программу, которая вычисляет математическое выражение по исходны*");
+            Console.WriteLine("*ым значениям данных, вводимых пользователем. Ответ округлите до 3 знаков *");
+            Console.WriteLine("* после запятой.                                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            double x, y;
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            double result = ds.Calculate(x, y);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}

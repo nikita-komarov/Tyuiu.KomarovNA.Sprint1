d71fbef75aa40ae06b1948b7317d5008010034a4
gravity34031 <gravity2507@gmail.com>
2024-09-20 21:34:01 +0500

    task2

--- Tyuiu.KomarovNA.Sprint1.Task2.V7/Program.cs
+++ Tyuiu.KomarovNA.Sprint1.Task2.V7/Program.cs
@@ -41,7 +41,7 @@ namespace Tyuiu.KomarovNA.Sprint1.Task2.V7
             DataService ds = new DataService();
 
             int r = int.Parse(Console.ReadLine());
-            double result = ds.CalculateSquareCircle(r);
+            int result = ds.CalculateSquareCircle(r);
             Console.WriteLine(result);
             Console.ReadKey();
         }

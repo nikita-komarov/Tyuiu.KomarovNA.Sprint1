e5bf0c14e058f25b646f5e2e9c0a6232f3dc63c3
gravity34031 <gravity2507@gmail.com>
2024-09-20 21:36:25 +0500

    1

--- Tyuiu.KomarovNA.Sprint1.Task2.V7.Lib/DataService.cs
+++ Tyuiu.KomarovNA.Sprint1.Task2.V7.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.KomarovNA.Sprint1.Task2.V7.Lib
     {
         public int CalculateSquareCircle(double value)
         {
-            return (int)Math.Round((Math.PI * Math.Pow(value, 2)), 3);
+            return (int)(Math.PI * Math.Pow(value, 2));
         }
     }
 }
